b4dd8626760629a6473d7534395b227e42c403b1
motsodumade <motsodumade@gmail.com>
2024-05-13 11:42:44 +0100

    userId needs updating at the end of newGUI.PHP

--- Data Analytics/newGUI.php
+++ Data Analytics/newGUI.php
@@ -189,8 +189,8 @@ $userId = $_SESSION["user"] -> id ?? null;
 
     <!-- Bootstrap JS and jQuery -->
     <!-- Bootstrap JS and jQuery -->
-    <script>var userId = <?php// echo json_encode($userId); ?>;</script> 
-    <!-- <script>var userId = 1 </script>-->
+    <script>var userId = <?php echo json_encode($userId); ?>;</script> 
+    <script>var userId = 1 </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
